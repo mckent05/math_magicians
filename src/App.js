48eda0adf8c964c392ecb89ed11a17ecc989c66d
MCKENT05 <atemitope95@gmail.com>
2021-11-24 10:52:09 +0100

    added events to make calculator functional

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import './App.css';
 import React, { Component } from 'react';
 import Calculator from './Component/Calculator';
+import calculate from './Logic/calculate';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      result: 10,
+      result: 0,
+      Obj: {
+      },
     };
   }
 
-  onclick() {
-    this.setState((prevState) => {
-      console.log('i got clicked');
-      return ({ result: prevState });
-    });
+  onclick = (e) => {
+    this.setState((prevState) => ({ Obj: calculate(prevState.Obj, e.target.dataset.id) }));
+    this.setState((prevState) => ({ result: this.showResult(prevState.Obj, e.target.dataset.id) }));
+  }
+
+  showResult = (object, btnName) => {
+    if (object.total && !object.operation && !object.next) {
+      return object.total;
+    } if (btnName === 'AC') {
+      return 0;
+    } if ('+-x÷%'.includes(btnName)) {
+      return object.total;
+    }
+    return object.next || object.total;
   }
 
   render() {
     const result = this.state;
     return (
       <div className="App d-flex j-center a-center">
-        <Calculator result={result.result} clickEvent={this.onclick} />
+        <Calculator result={result.result} clickEvent={(event) => this.onclick(event)} />
       </div>
     );
   }
